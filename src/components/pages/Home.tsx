db596a304d802054e900a50dc048a6afb7333a2b
Dylan <d.bullock1404@outlook.com>
2025-03-10 12:08:03 +0000

    fixed darkmode icons showing on lightmode

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -54,17 +54,17 @@ export default function Home() {
                     rel="noreferrer"
                     target="_blank">
                     <p>Let's Talk</p>
-                    <svg className="w-7 fill-white" viewBox="0 -960 960 960" xmlns="http://www.w3.org/2000/svg">
+                    <svg className="w-7 fill-onyxBlack dark:fill-almostWhite" viewBox="0 -960 960 960" xmlns="http://www.w3.org/2000/svg">
                         <path d="m560-240-56-58 142-142H160v-80h486L504-662l56-58 240 240-240 240Z"/>
                     </svg>
                 </a>
 
                 <a
-                    className="w-fit group flex flex-row gap-1 p-4 border-2 border-midPurple hover:border-darkPurple rounded-xl backdrop-blur-sm text-sm sm:text-lg font-bold font-fira-code hover:scale-120 transition-all duration-300"
+                    className="w-fit group flex flex-row gap-1 items-center p-4 border-2 border-midPurple hover:border-darkPurple rounded-xl backdrop-blur-sm text-sm sm:text-lg font-bold font-fira-code hover:scale-120 transition-all duration-300"
                     href="/media/pdfs/CV-Dylan-Bullock.pdf"
                     target="_blank"
                     download="CV-Dylan-Bullock.pdf">
-                    <svg className="w-7 fill-white group-hover:fill-darkPurple transition-all duration-initial" viewBox="0 -960 960 960" xmlns="http://www.w3.org/2000/svg">
+                    <svg className="w-7 fill-onyxBlack dark:fill-almostWhite group-hover:fill-darkPurple transition-all duration-initial" viewBox="0 -960 960 960" xmlns="http://www.w3.org/2000/svg">
                         <path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"/>
                     </svg>
                     <p className="group-hover:text-darkPurple transition-all duration-300">Download CV</p>
