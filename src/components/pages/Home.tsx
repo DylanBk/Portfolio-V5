b206e7d89d2bcf7ab13de52eba33bb51f8e23e55
DylanBk <d.bullock1404@outlook.com>
2025-03-13 22:47:54 +0000

    changed font size on typing section for larger phones

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -44,20 +44,20 @@ export default function Home() {
 
             <div className="h-screen -z-10 fixed dot-grid"></div>
 
-            <div className="h-full w-fit z-10 ml-7 mt-56 sm:mt-56 md:mt-56 xl:mt-64 text-lg sm:text-2xl lg:text-3xl xl:text-4xl font-bold dark:text-white select-none smooth-change">
+            <div className="h-full w-fit z-10 ml-7 mt-56 sm:mt-56 md:mt-56 xl:mt-64 text-lg xs:text-xl sm:text-2xl lg:text-3xl xl:text-4xl font-bold dark:text-white select-none smooth-change">
                 <h2>Hey there, my name's <span className="text-midBlue">Dylan.</span></h2>
                 <h2>I'm a <span
                     id="slow-type-section"
                     className="bg-gradient-to-r from-midBlue to-midPurple bg-clip-text font-bold text-transparent"
                     ref={typingRef} />
                 </h2>
-                <h2>based in the <span className="text-darkBlue font-bold">United Kingdom</span></h2>
+                <h2>based in the <span className="text-darkBlue font-bold">United Kingdom.</span></h2>
             </div>
 
             <div className="w-fit z-10 flex flex-row gap-7 mx-auto sm:mx-0 sm:pl-7 mt-28 text-nowrap text-onyxBlack dark:text-white">
                 <a
                     id="primary-cta"
-                    className="w-fit flex flex-row gap-1 items-center p-4 rounded-xl text-sm sm:text-lg font-bold font-fira-code"
+                    className="w-fit flex flex-row gap-1 items-center px-4 py-3 sm:py-4 rounded-xl text-sm sm:text-lg font-bold font-fira-code"
                     href="mailto:dylanbullock.dev@gmail.com"
                     rel="noreferrer"
                     target="_blank">
@@ -68,7 +68,7 @@ export default function Home() {
                 </a>
 
                 <a
-                    className="w-fit group flex flex-row gap-1 items-center p-4 border-2 border-midPurple hover:border-darkPurple rounded-xl backdrop-blur-sm text-sm sm:text-lg font-bold font-fira-code hover:scale-120 transition-all duration-300"
+                    className="w-fit group flex flex-row gap-1 items-center px-4 py-3 sm:py-4 border-2 border-midPurple hover:border-darkPurple rounded-xl backdrop-blur-sm text-sm sm:text-lg font-bold font-fira-code hover:scale-120 transition-all duration-300"
                     href="/media/pdfs/CV-Dylan-Bullock.pdf"
                     target="_blank"
                     download="CV-Dylan-Bullock.pdf"
